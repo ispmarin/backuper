23f60184beb4c94c9d3e9be81a2deaa0bbf181c5
Ivan Marin <ivan@ivanmarin.cc>
2019-11-03 20:40:32 -0300

    start adding job flow control

--- backuper/backuper.py
+++ backuper/backuper.py
@@ -2,19 +2,21 @@
 
 import falcon
 
-from interfaces import CreateBackup, ListBackups
+from interfaces import CreateBackup, ListBackups, CheckBackupProgress
 import rq
 import redis
 
 
 redis_con = redis.Redis()
-backup_queue = rq.Queue(connection=redis_con)
+backup_queue = rq.Queue(connection=redis_con, timeout='6h')
 
 app = falcon.API()
 create_backup = CreateBackup(backup_queue)
 list_backup = ListBackups('gdrive_usp', 'backup')
+check_backup_progress = CheckBackupProgress(backup_queue)
 
-app.add_route('/list', list_backup)
+app.add_route('/list_backups', list_backup)
 app.add_route('/create', create_backup)
+app.add_route('/check', create_backup)
 
 
--- backuper/interfaces.py
+++ backuper/interfaces.py
@@ -1,4 +1,6 @@
 import falcon
+import uuid
+
 from controllers import list_backups, create_backup
 
 
@@ -11,14 +13,24 @@ class CreateBackup(object):
         resp.body = ('Answer')
 
     def on_post(self, req, resp):
+
+        job_id = uuid.uuid4().hex
         backup_folder = req.media.get('backup_folder')
         remote_gdrive = req.media.get('remote_gdrive')
         remote_folder = req.media.get('remote_folder')
         passphrase    = req.media.get('passphrase')
-        backup_job = self.backup_queue.enqueue(create_backup, args=(backup_folder, remote_gdrive, remote_folder, passphrase), timeout='6h')
+        backup_job = self.backup_queue.enqueue(
+            create_backup, 
+            args=(backup_folder, remote_gdrive, remote_folder, passphrase,),
+            kwags={
+                'description':'Backup process: folder {} gdrive {}'.format(backup_folder, remote_folder),
+                'job_id': job_id
+            },
+            job_timeout='6h'
+        )
 
         if backup_job.result is None:
-            resp.body = ('Backup process started')
+            resp.body = ('{}'.format(job_id))
         else:
             resp.status = falcon.status_codes.HTTP_500
             resp.body = ('Failure on queue')
@@ -41,3 +53,38 @@ class ListBackups(object):
         except Exception as e:
             resp.status = falcon.status_codes.HTTP_500
             resp.body = ("Error accessing Gdrive backups, {}".format(e))
+
+
+class CheckBackupProgress(object):
+
+    def __init__(self, backup_queue):
+        self.backup_queue = backup_queue
+
+    def on_post(self, req, resp):
+
+        try:
+            job_id = req.media.get('job_id')
+            job_checked = self.backup_queue.fetch_job(job_id)
+            
+            if job_checked == "Done":
+                resp.body = {"job_id :{} \n {} \n Done".format(job_id, job_checked.description)}
+            else:
+                resp.body = ("Still running. \n")
+        except Exception as e:
+            resp.status = falcon.status_codes.HTTP_500
+            resp.body = ("Error {}".format(e))
+
+class GetJobIDs(object):
+
+    def __init__(self, backup_queue):
+        self.backup_queue = backup_queue
+
+    def on_get(self, req, resp):
+        try:
+            jobs_list = self.backup_queue.job_ids
+            jobs_count = self.backup_queue.count
+            
+            resp.body = {"job_ids :{} \n Total jobs in queue {}".format(jobs_list, jobs_count)}
+        except Exception as e:
+            resp.status = falcon.status_codes.HTTP_500
+            resp.body = ("Error {}".format(e))
